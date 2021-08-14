d271026922623c37c75ac73766be2f396a76fb07
m-a-u-d <maudfreeman@gmail.com>
2021-08-14 11:43:50 +1000

    o

--- img2txt.py
+++ img2txt.py
@@ -7,28 +7,24 @@ import cv2
 import numpy as np
 
 
-def get_args():
-    parser = argparse.ArgumentParser("Image to ASCII")
-    parser.add_argument("--input", type=str, default="data/input.jpg", help="Path to input image")
-    parser.add_argument("--output", type=str, default="data/output.txt", help="Path to output text file")
-    parser.add_argument("--mode", type=str, default="complex", choices=["simple", "complex"],
-                        help="10 or 70 different characters")
-    parser.add_argument("--num_cols", type=int, default=150, help="number of character for output's width")
-    args = parser.parse_args()
-    return args
+mode = "simple"
+num_cols = 10000
+img_path = "elevation thermo_1_500000.jpg"
+out_path = "thermo_1000.txt"
 
 
-def main(opt):
-    if opt.mode == "simple":
+
+def main(mode,num_cols, img_path, out_path):
+    if mode == "simple":
         CHAR_LIST = '@%#*+=-:. '
     else:
         CHAR_LIST = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
     num_chars = len(CHAR_LIST)
-    num_cols = opt.num_cols
-    image = cv2.imread(opt.input)
+    num_cols = num_cols
+    image = cv2.imread(img_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     height, width = image.shape
-    cell_width = width / opt.num_cols
+    cell_width = width / num_cols
     cell_height = 2 * cell_width
     num_rows = int(height / cell_height)
     if num_cols > width or num_rows > height:
@@ -38,8 +34,9 @@ def main(opt):
         num_cols = int(width / cell_width)
         num_rows = int(height / cell_height)
 
-    output_file = open(opt.output, 'w')
+    output_file = open(out_path, 'w')
     for i in range(num_rows):
+        print("row "+str(i)+" of: "+str(num_rows))
         for j in range(num_cols):
             output_file.write(
                 CHAR_LIST[min(int(np.mean(image[int(i * cell_height):min(int((i + 1) * cell_height), height),
@@ -50,5 +47,6 @@ def main(opt):
 
 
 if __name__ == '__main__':
-    opt = get_args()
-    main(opt)
+    
+    main(mode,num_cols, img_path, out_path)
+    print("finished")
